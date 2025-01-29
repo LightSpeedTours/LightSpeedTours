787538c63a689c14d981c2ae803177445ddd115a
Scrat112403 <jacarmonaa@unal.edu.co>
2025-01-28 22:09:28 -0800

    Basics
    Elementos basicos listos, falta la interactividad y darle estilo con CSS

--- speed-light-tours-api/src/app.ts
+++ speed-light-tours-api/src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import routes from './routes';
+import Hospedajes from './pages/hospedajes';
 
 const app: Application = express();
 
