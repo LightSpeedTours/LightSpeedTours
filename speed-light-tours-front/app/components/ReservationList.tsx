b525ec55b653d915702cc32764bf77910a997d1b
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-02-01 21:57:13 -0500

    Add ReservationsPage and ReservationList components with styling updates

---
+++ speed-light-tours-front/app/components/ReservationList.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import ReservationCard from "./ReservationCard";
+import '../app.css';
+
+const ReservationList: React.FC = () => (
+  <div className="reservation-list">
+    <h1 className="reservation-list-title">Mis Reservas</h1>
+    <ReservationCard
+        service="Servicio 1"
+        price="$100"
+        description="Descripción del servicio 1"
+        location="Ubicación 1"
+        dates="01/01/2025 - 02/01/2025"
+        people="2"
+        countDown="3 días"
+        timeLimitModify="3 días antes"
+        timeLimitCancel="1 semana antes"
+        imageSrc=""
+    />
+    <ReservationCard
+        service="Servicio 2"
+        price="$200"
+        description="Descripción del servicio 2"
+        location="Ubicación 2"
+        dates="03/01/2025 - 04/01/2025"
+        people="4"
+        countDown="3 días"
+        timeLimitModify="3 días antes"
+        timeLimitCancel="1 semana antes"
+        imageSrc=""
+    />
+    <ReservationCard
+        service="Servicio 3"
+        price="$300"
+        description="Descripción del servicio 3"
+        location="Ubicación 3"
+        dates="05/01/2025 - 06/01/2025"
+        people="6"
+        countDown="3 días"
+        timeLimitModify="3 días antes"
+        timeLimitCancel="1 semana antes"
+        imageSrc=""
+    />
+  </div>
+);
+
+export default ReservationList;
---
+++ speed-light-tours-front/app/routes/ReservationsPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import ReservationList from "../components/ReservationList";
+
+// ReservationsPage Component
+const ReservationsPage: React.FC = () => (
+  <div>
+    <Header />
+    <ReservationList />
+  </div>
+);
+
+export default ReservationsPage;
