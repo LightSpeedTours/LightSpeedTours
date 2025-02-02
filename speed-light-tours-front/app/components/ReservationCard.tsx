685a618ca0938898345b19c1d8dd8d4ec17bfd30
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-02-01 22:36:22 -0500

    Add Modal functionality to ReservationCard for modifying reservations

--- speed-light-tours-front/app/components/ReservationCard.tsx
+++ speed-light-tours-front/app/components/ReservationCard.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 import Card from "./Card";
 import Button from "./Button";
 import '../app.css';
@@ -27,36 +28,47 @@ const ReservationCard: React.FC<ReservationCardProps> = ({
     timeLimitModify,
     timeLimitCancel,
     imageSrc,
-}) => (
-    <Card className="reservation-card">
-        <div className="reservation-card-left">
-            <div className="service-info">
-                <h6 className="service-title">{service}</h6>
-                <img src={imageSrc} alt={service} className="service-image" />
+}) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    return (
+        <>
+            <Card className="reservation-card">
+            <div className="reservation-card-left">
+                <div className="service-info">
+                    <h6 className="service-title">{service}</h6>
+                    <img src={imageSrc} alt={service} className="service-image" />
+                </div>
+
+                <div className="price-description">
+                    <p className="price">Precio: {price}</p>
+                    <p className="description">{description}</p>
+                </div>
             </div>
 
-            <div className="price-description">
-                <p className="price">Precio: {price}</p>
-                <p className="description">{description}</p>
+            <div className="reservation-info">
+                <p>Ubicación: {location}</p>
+                <p>Fechas: {dates}</p>
+                <p>Cantidad de personas: {people}</p>
             </div>
-        </div>
 
-        <div className="reservation-info">
-            <p>Ubicación: {location}</p>
-            <p>Fechas: {dates}</p>
-            <p>Cantidad de personas: {people}</p>
-        </div>
-
-        <div className="reservation-card-right">
-            <p className="countdown">{countDown}</p>
-            <Button className="modify-button" variant="outline">
-                Modificar reserva
-            </Button>
-            <Button variant="destructive">Cancelar</Button>
-            <br /><p className="time-limit">Válido hasta: <br />{timeLimitModify}</p>
-            <p className="time-limit">Válido hasta: <br />{timeLimitCancel}</p>                   
-        </div>
-    </Card>
-);
+            <div className="reservation-card-right">
+                <p className="countdown">{countDown}</p>
+                <Button className="modify-button" variant="outline" onClick={openModal}>
+                    Modificar reserva
+                </Button>
+                <Button variant="destructive">
+                    Cancelar
+                </Button>
+                <br /><p className="time-limit">Válido hasta: <br />{timeLimitModify}</p>
+                <p className="time-limit">Válido hasta: <br />{timeLimitCancel}</p>
+            </div>
+            </Card>
+            <Modal isOpen={isModalOpen} onClose={closeModal} />
+        </>    
+    );
+};
 
 export default ReservationCard;
