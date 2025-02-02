615c116cad29d5c5bb245d88ce2abe9cc63779c3
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-02-01 21:50:50 -0500

    Fix formatting in Header component CSS import

--- speed-light-tours-front/app/components/Header.tsx
+++ speed-light-tours-front/app/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "./Button";
-import '../app.css'; // Import the CSS file
+import '../app.css';
 
 // Header Component
 const Header: React.FC = () => (
