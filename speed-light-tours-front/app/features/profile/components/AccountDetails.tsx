e3537b79cc53e09ff97ed7dd8ec2877dec596370
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-02-13 15:51:56 -0500

    Enhance account details layout with grid structure and improved styling

--- speed-light-tours-front/app/features/profile/components/AccountDetails.tsx
+++ speed-light-tours-front/app/features/profile/components/AccountDetails.tsx
@@ -13,10 +13,14 @@ const AccountDetails: React.FC<AccountDetailsProps> = ({ username, email, passwo
     <div className="account-details">
       <h3><strong>Datos de cuenta</strong></h3>
       <div className="account-grid">
-          <div>Nombre de usuario: {username}</div>
-          <div>Correo: {email}</div>
-          <div>Contraseña: {password}</div>
-          <div>Teléfono: {phone}</div>
+        <div className="account-title">Nombre de usuario:</div>
+        <div className="account-info">{username}</div>
+        <div className="account-title">Correo:</div>
+        <div className="account-info">{email}</div>
+        <div className="account-title">Contraseña:</div>
+        <div className="account-info">{password}</div>
+        <div className="account-title">Teléfono:</div>
+        <div className="account-info">{phone}</div>
       </div>
     </div>
   );
