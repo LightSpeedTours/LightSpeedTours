84b3845e2a0f784e059e42679be1050321cb8fdb
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2025-03-06 22:25:53 -0500

    Refactor profile components: remove unused image, create AccountDetailsProps interface, implement EditModal for profile editing, and enhance PersonalInfo with modal functionality

---
+++ speed-light-tours-front/app/features/profile/utils/types.ts
@@ -0,0 +1,10 @@
+export interface AccountDetailsProps {
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+    date: string;
+    gender: string;
+    occupation: string;
+    profileImage: string;
+  }
